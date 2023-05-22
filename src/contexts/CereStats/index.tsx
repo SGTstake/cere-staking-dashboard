b622077c5d6d170773770d81550d1014ce334ae6
shamilkhan <shamilkhan1999@gmail.com>
2023-05-22 17:58:57 +0400

    Adjust Payouts request

--- src/contexts/CereStats/index.tsx
+++ src/contexts/CereStats/index.tsx
@@ -3,11 +3,15 @@ import {
   ApolloClient,
   NormalizedCacheObject,
   InMemoryCache,
+  gql,
 } from '@apollo/client';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { defaultCereStatsContext } from './defaults';
 import { CereStatsContextInterface } from './types';
 import { useConnect } from '../Connect';
+import { useApi } from '../Api';
+import { UIContextInterface } from '../UI/types';
+import { useUi } from '../UI';
 
 const CereStatsContext = createContext<CereStatsContextInterface>(
   defaultCereStatsContext
@@ -20,32 +24,30 @@ export const CereStatsProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  const { network, isReady } = useApi();
+  const { services }: UIContextInterface = useUi();
   const { activeAccount } = useConnect();
-  console.log('activeAccount', activeAccount);
 
   const [client, setClient] =
     useState<ApolloClient<NormalizedCacheObject> | null>(null);
   const [payouts, setPayouts] = useState([]);
 
-  // replace with your era points GraphQL query
-  const ERA_POINTS_QUERY = `
-      subscription EraPoints($stashAddress: String) {
-        era_points(
-          where: { stash_address: { _eq: $stashAddress } }
-        ) {
-          stash_address
-          era
-          points
-        }
-      }
-    `;
+  // reset payouts on network switch
+  useEffect(() => {
+    setPayouts([]);
+  }, [network]);
 
-  // replace with your payouts GraphQL query
-  const PAYOUTS_QUERY = `
-    query FetchPayouts {
-      //...
+  // fetch payouts as soon as network is ready
+  useEffect(() => {
+    if (isReady) {
+      fetchPayouts();
     }
-  `;
+  }, [isReady, network, activeAccount]);
+
+  // fetch payouts on services toggle
+  useEffect(() => {
+    fetchPayouts();
+  }, [services]);
 
   useEffect(() => {
     const wsLink = new WebSocketLink({
@@ -68,6 +70,19 @@ export const CereStatsProvider = ({
       return [];
     }
 
+    // replace with your era points GraphQL query
+    const ERA_POINTS_QUERY = `
+      subscription EraPoints($stashAddress: String) {
+        era_points(
+          where: { stash_address: { _eq: $stashAddress } }
+        ) {
+          stash_address
+          era
+          points
+        }
+      }
+    `;
+
     const { data } = await client.query({
       // @ts-ignore
       query: ERA_POINTS_QUERY,
@@ -77,15 +92,29 @@ export const CereStatsProvider = ({
     return data.eraPoints; // replace with the actual key in the response
   };
 
-  const fetchPayouts = async (address: string) => {
-    if (!address || !client) {
+  const fetchPayouts = async () => {
+    if (!activeAccount || !client) {
       return;
     }
 
     const { data } = await client.query({
-      // @ts-ignore
-      query: PAYOUTS_QUERY,
-      variables: { address },
+      query: gql`
+        query Event($accountId: String!) {
+          event(
+            order_by: { block_number: desc }
+            where: {
+              section: { _eq: "staking" }
+              method: { _like: "Reward%" }
+              data: { _like: $accountId }
+            }
+          ) {
+            block_number
+            data
+            timestamp
+          }
+        }
+      `,
+      variables: { accountId: activeAccount },
     });
 
     setPayouts(data.payouts); // replace with the actual key in the response
