25feb54b5530e758693c25856ca266f3749e738b
shamilkhan <shamilkhan1999@gmail.com>
2023-07-11 17:16:22 +0400

    Remove extra code.

--- src/library/ValidatorList/Validator/types.ts
+++ src/library/ValidatorList/Validator/types.ts
@@ -1,6 +1,5 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-import { DOMElement } from 'react';
 
 export interface BlockedProps {
   prefs: any;
