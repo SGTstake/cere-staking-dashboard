7792896fabd0c3782e2c05fe8337ad7b0300ccb2
bulgakovvlad <30372241+bulgakovvlad@users.noreply.github.com>
2023-10-10 12:46:10 +0300

    Update index.ts
    Added "AnyValid" identity section below "Cere" section

--- src/config/validators/index.ts
+++ src/config/validators/index.ts
@@ -5,9 +5,30 @@
  * Use upper camel-case for your SVG import, lower camel case for the svg.
  * import { ReactComponent as ValidatorEntityName } from './thumbnails/validatorEntityName.svg';
  */
+import { ReactComponent as Cere } from './thumbnails/cere.svg';
 import { ReactComponent as AnyValid } from './thumbnails/anyvalid.svg';
 
 export const VALIDATOR_COMMUNITY = [
+  {
+    name: 'CERE',
+    Thumbnail: Cere,
+    bio: `Official Validators from Cere Network, the world's first Decentralized Data Cloud platform.`,
+    email: 'team@cere.network',
+    website: 'https://cere.network',
+    twitter: '@CereNetwork',
+    validators: {
+      cere: [
+        '6S4mrsCrqWoBAYrp2PKQNh7CYcCtyEtYpx5J626Kj5vszSyy',
+        '6QhzyvZQm3dLjDmeaoUnLPXzfuTi6X1HEo6AX6gfVbC3shzD',
+        '6RgfwDiQTLjgbkQ5CorrKtRtCaDABQKYsibk9MeyvzmKFrk2',
+        '6TBhZAgtFc3Wr8BeNu5tdMJG1NDpxKbG2Hwf2UbVtMGyFxzN',
+        '6Pyh9zZgp4XCP338VDG7oshK7PvsAdyuBN6S2NNm7CBoCXx8',
+        '6S9tXQmPYoeBXYey8vKYi9BMbNMD8Zgqb62k7SYMNQLUbydZ',
+        '6PwAv2L43zGPEwHTb1L7LyCWv7yq2Hc4dSVYHvvi1kscCR91',
+        '6Qshjra42mLDtc9ouHzUz1bMmYXg2qasmW2xSLgendRdsYED',
+      ],
+    },
+  },
   {
     name: 'AnyValid',
     Thumbnail: AnyValid,
@@ -16,9 +37,7 @@ export const VALIDATOR_COMMUNITY = [
     website: 'https://anyvalid.com',
     twitter: '@anyvalid',
     validators: {
-      cere: [
-        '6UDVCKB9opndqcRAxTpTvKFTFXUwvE36aYnp1bNyVV3Cfh16',
-      ],
+      cere: ['6UDVCKB9opndqcRAxTpTvKFTFXUwvE36aYnp1bNyVV3Cfh16'],
     },
   },
 ];
